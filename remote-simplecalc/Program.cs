9a9d680837ae60f508a32ff28160f72bac6ebbe1
k1ark <shokolad2345@gmail.com>
2024-09-16 21:22:57 +0500

    Added Sum, divide, subtraction and multiply writeline.

--- remote-simplecalc/Program.cs
+++ remote-simplecalc/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SimpleCalculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Простой калькулятор:");
+        Console.WriteLine("1. Сложение");
+        Console.WriteLine("2. Вычитание");
+        Console.WriteLine("3. Умножение");
+        Console.WriteLine("4. Деление");
+
+    }
