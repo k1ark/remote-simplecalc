ace75f80c832fa58d84fe722b0e5b98fa8487034
k1ark <shokolad2345@gmail.com>
2024-09-16 21:24:35 +0500

    Added if condition with 2 nums.

--- remote-simplecalc/Program.cs
+++ remote-simplecalc/Program.cs
@@ -10,4 +10,15 @@ class SimpleCalculator
         Console.WriteLine("3. Умножение");
         Console.WriteLine("4. Деление");
 
+        Console.Write("Выберите операцию (1/2/3/4): ");
+        string choice = Console.ReadLine();
+
+        if (choice == "1" || choice == "2" || choice == "3" || choice == "4")
+        {
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+        }
     }
