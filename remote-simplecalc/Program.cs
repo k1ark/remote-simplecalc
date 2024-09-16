c1d462c9a1ec777f559c59f8860f2c0feddf12a3
k1ark <shokolad2345@gmail.com>
2024-09-16 22:54:50 +0500

    added brakes after resolving conflict.

--- remote-simplecalc/Program.cs
+++ remote-simplecalc/Program.cs
@@ -9,41 +9,6 @@ class SimpleCalculator
         Console.WriteLine("2. Вычитание");
         Console.WriteLine("3. Умножение");
         Console.WriteLine("4. Деление");
-
-        Console.Write("Выберите операцию (1/2/3/4): ");
-        string choice = Console.ReadLine();
-
-        if (choice == "1" || choice == "2" || choice == "3" || choice == "4")
-        {
-            Console.Write("Введите первое число: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Введите второе число: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            switch (choice)
-            {
-                case "1":
-                    Console.WriteLine($"Результат: {num1 + num2}");
-                    break;
-                case "2":
-                    Console.WriteLine($"Результат: {num1 - num2}");
-                    break;
-                case "3":
-                    Console.WriteLine($"Результат: {num1 * num2}");
-                    break;
-                case "4":
-                    if (num2 != 0)
-                    {
-                        Console.WriteLine($"Результат: {num1 / num2}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ошибка: деление на ноль!");
-                    }
-                    break;
-            }
-        }
-       
     }
 }
+ 
