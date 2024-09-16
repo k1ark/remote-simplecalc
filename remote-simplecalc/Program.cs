84b29947a939472afc2203e848c222bc4360a231
k1ark <shokolad2345@gmail.com>
2024-09-16 22:55:40 +0500

    returned to beginning.

--- remote-simplecalc/Program.cs
+++ remote-simplecalc/Program.cs
@@ -1,14 +1,2 @@
-﻿using System;
-
-class SimpleCalculator
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Простой калькулятор:");
-        Console.WriteLine("1. Сложение");
-        Console.WriteLine("2. Вычитание");
-        Console.WriteLine("3. Умножение");
-        Console.WriteLine("4. Деление");
-    }
-}
- 
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
